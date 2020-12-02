1b30db76776e2e3d8d541322306f9cd1a0e1d4c1
mtrimner <matt.trimner@gmail.com>
2020-12-02 11:47:03 -0800

    commit tree 157bac5a47aa267fb8dd7cbdcc4cbf1a6b840ac6

--- index.js
+++ index.js
@@ -29,9 +29,49 @@ function createTimeInEvent(employee, dateStamp){
 function createTimeOutEvent(employee, dateStamp){
     let [date, hour] = dateStamp.split(" ")
     employee.timeOutEvents.push({
-        type: "timeOut",
+        type: "TimeOut",
         hour: parseInt(hour, 10),
         date: date
     })
     return employee
+}
+
+function hoursWorkedOnDate(employee, formDate){
+   let timeIn = employee.timeInEvents.find(function(e){
+        return e.date === formDate
+    })
+
+    let timeOut = employee.timeOutEvents.find(function(e){
+        return e.date === formDate
+    })
+    return (timeOut.hour - timeIn.hour) /100
+}
+
+function wagesEarnedOnDate(employee, date){
+    let hoursWorked = hoursWorkedOnDate(employee, date) * employee.payPerHour
+    return hoursWorked
+}
+
+function allWagesFor(employee){
+    let allDates = employee.timeInEvents.map(function(e){
+        return e.date
+    })
+
+    let totalPay = allDates.reduce(function(acc, dates){
+        return acc + wagesEarnedOnDate(employee, dates)
+    }, 0)
+    
+    return totalPay
+}
+
+function findEmployeeByFirstName(srcArray, firstName){
+    return srcArray.find(function(e){
+        return e.firstName === firstName
+    })
+}
+
+function calculatePayroll(array){
+    return array.reduce(function(acc, e){
+        return acc + allWagesFor(e)
+    }, 0)
 }
