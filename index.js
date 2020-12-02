2186a0d7ad91b9996c27333cb68638188def96d2
mtrimner <matt.trimner@gmail.com>
2020-12-02 11:09:13 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Your code here
+function createEmployeeRecord(array) {
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(employees) {
+    return employees.map(function(employee) {
+        return createEmployeeRecord(employee)
+    })
+}
+
+function createTimeInEvent(employee, dateStamp){
+    let [date, hour] = dateStamp.split(" ")
+    employee.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date: date
+    })
+
+    return employee
+}
+
+function createTimeOutEvent(employee, dateStamp){
+    let [date, hour] = dateStamp.split(" ")
+    employee.timeOutEvents.push({
+        type: "timeOut",
+        hour: parseInt(hour, 10),
+        date: date
+    })
+    return employee
+}
